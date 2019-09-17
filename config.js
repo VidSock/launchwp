96a912c88691ca0faed757de63c50c9872244d3e
Todd <ihatespam@gmail.com>
2019-09-17 15:07:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@
  * mailChimpEndpoint see https://github.com/benjaminhoffman/gatsby-plugin-mailchimp#mailchimp-endpoint
  */
 const config = {
-  title: `Urban Fetish - Abandoned Urbex Urban Exploration Photos and Locations`,
-  description: `Urban Fetish`,
-  author: `Urban Fetish`,
+  title: `Twilightscapes - Abandoned Urbex Urban Exploration Photos and Locations`,
+  description: `Twilightscapes`,
+  author: `Twilightscapes`,
   twitter: `toddlambert`,
-  wordPressUrl: `https://launchlamp.com/urbanfetish`,
-  siteUrl: `https://urbanfetish.com`,
+  wordPressUrl: `https://twilightscapes.com`,
+  siteUrl: `https://twilightscapes.com`,
   dynamicComments: true,
   gaTrackingId: 'UA-140479230-1',
   mailChimpEndpoint: '',
